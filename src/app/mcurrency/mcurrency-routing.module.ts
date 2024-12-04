1591eefec008121ff646db3a266d7e8448192cc6
bravosty <stephenbravo58@gmail.com>
2024-12-04 22:25:32 +0300

    routing works

--- src/app/mcurrency/mcurrency-routing.module.ts
+++ src/app/mcurrency/mcurrency-routing.module.ts
@@ -3,27 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { MconfigurationComponent } from './mconfiguration/mconfiguration.component';
 import { EntryComponent } from './entry/entry.component';
 import { ReportingComponent } from './reporting/reporting.component';
-import {Route} from '../core/route/route.service'
 
 const routes: Routes = [
-  Route.withShell([
-    {
+  {
     path: '',
-    redirectTo: '/',
+    redirectTo: 'configuration',
     pathMatch: 'full',
   },
-    {
-      path: 'mutli-currency/configuration',
-      component: MconfigurationComponent
-    },
-    {
-      path: 'multi-currency/entry',
-      component: EntryComponent
-    },
-    {
-      path: 'multi-currency/reporting',
-      component: ReportingComponent
-    }])
+  {
+    path: 'configuration',
+    component: MconfigurationComponent
+  },
+  {
+    path: 'entry',
+    component: EntryComponent
+  },
+  {
+    path: 'reporting',
+    component: ReportingComponent
+  }
 ];
 
 @NgModule({
--- src/app/mcurrency/mcurrency.module.ts
+++ src/app/mcurrency/mcurrency.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { McurrencyRoutingModule } from './mcurrency-routing.module';
 import { McurrencyComponent } from './mcurrency.component';
+import { MconfigurationComponent } from './mconfiguration/mconfiguration.component';
+import { EntryComponent } from './entry/entry.component';
+import { ReportingComponent } from './reporting/reporting.component';
 
 
 @NgModule({
   declarations: [
-    McurrencyComponent
+    McurrencyComponent,
+    MconfigurationComponent,
+    EntryComponent,
+    ReportingComponent,
   ],
   imports: [
     CommonModule,
-    McurrencyRoutingModule
+    McurrencyRoutingModule,
   ]
 })
 export class McurrencyModule { }
